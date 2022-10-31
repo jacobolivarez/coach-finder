b446484583b70e40aa18e462f841c9771f62aa02
Jacob Olivarez <jacob.olivarez@gmail.com>
2022-10-31 18:39:21 -0500

    Added auth ui.

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import List from './pages/coaches/List';
 import Register from './pages/coaches/Register';
 import ContactCoach from './pages/requests/ContactCoach';
 import RequestsReceived from './pages/requests/RequestsReceived';
+import UserAuth from './pages/auth/UserAuth';
 import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
@@ -19,6 +20,7 @@ const router = createRouter({
     },
     { path: '/register', component: Register },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
