d8c8892d5fa26dc750311b56abb9b8987db1dd79
Jacob Olivarez <jacob.olivarez@gmail.com>
2022-11-01 07:47:10 -0500

    Added login action

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -62,14 +62,15 @@ export default {
         return;
       }
       this.isLoading = true;
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
       try {
         if (this.mode === 'login') {
-          //
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
         } catch (error) {
           this.error = error.essage || 'Failed to authenticate. Try again later.'
